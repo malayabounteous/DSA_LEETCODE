8a493456aebbdb18e38b5e5bad35c0b630f5a2d4
malayabounteous <malaya.sahu@bounteous.com>
2026-02-11 21:29:02 +0530

    Time: 180 ms (23.16%), Space: 100 MB (66.09%) - LeetHub

---
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+        int ans=INT_MIN;
+        int n=nums.size();
+        for(int i=0;i<n/2;i++)
+        {
+            ans=max(ans,nums[i]+nums[n-i-1]);
+        }
+        return ans;
+    }
+};
