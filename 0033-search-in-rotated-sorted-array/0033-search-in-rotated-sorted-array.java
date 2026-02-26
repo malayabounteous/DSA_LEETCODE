59406b39e02f05023cb1520d2443b66330e9c6d6
Malaya Sahu <malaya.sahu@bounteous.com>
2026-02-26 17:55:28 +0530

    Time: 0 ms (100%), Space: 43.8 MB (83.03%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int n=nums.length;
+
+        int left=0;
+        int right=n-1;
+        int ans=-1;
+
+        while(left<=right)
+        {
+            int mid=(left+right)/2;
+             
+             if(nums[mid]==target)
+             {
+                ans=mid;
+                break;
+             }
+             else if(nums[left]<=nums[mid])
+             {
+                if(nums[left]<=target && nums[mid]>=target)
+                right=mid-1;
+                else
+                left=mid+1;
+             }
+             else
+             {
+                if(nums[mid]<=target && target<=nums[right])
+                {
+                    left=mid+1;
+
+                }
+                else
+                right=mid-1;
+             }
+
+        }
+        return ans;
+    }
+}
