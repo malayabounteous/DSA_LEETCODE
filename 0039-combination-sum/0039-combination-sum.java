3ad40c2278127912473399fcb71a489c7c71242b
Malaya Sahu <malaya.sahu@bounteous.com>
2026-03-01 11:55:22 +0530

    Time: 2 ms (95.28%), Space: 46 MB (33.65%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,5 +1,29 @@
 class Solution {
+    void generate(int i,int target,List<Integer>l,int[]candidates,List<List<Integer>>ans)
+    {
+        if(target==0)
+        {
+            ans.add(new ArrayList<>(l));
+            return;
+        }
+        if(target<0 || i>=candidates.length)
+        return;
+        
+        if(target>=candidates[i])
+        {
+            l.add(candidates[i]);
+            generate(i,target-candidates[i],l,candidates,ans);
+            l.remove(l.size()-1);
+        }
+        generate(i+1,target,l,candidates,ans);
+
+
+    }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         
+        List<List<Integer>>ans=new ArrayList<>();
+        List<Integer>t=new ArrayList<>();
+        generate(0,target,t,candidates,ans);
+        return ans;
     }
 }
