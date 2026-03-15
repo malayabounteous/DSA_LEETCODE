2e8d0495cbe08eb4e393ce763792049d79bb7d1d
Malaya Sahu <malaya.sahu@bounteous.com>
2026-03-15 23:24:00 +0530

    Time: 0 ms (100%), Space: 44.6 MB (90.62%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        
+        int n=nums.length;
+        int l=0,r=n-1;
+
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]<target)
+            {
+                l=mid+1;
+            }
+            else
+            {
+                r=mid-1;
+            }
+        }
+        return l;
+    }
+}
