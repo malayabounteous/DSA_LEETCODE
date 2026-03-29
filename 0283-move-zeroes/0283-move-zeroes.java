53ea271428bee268a3601994a29dfb5f449fabee
Malaya Sahu <malaya.sahu@bounteous.com>
2026-03-29 12:20:21 +0530

    Time: 2 ms (90.7%), Space: 47.9 MB (49.33%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int n=nums.length;
+        int left=0;
+        for(int i=0;i<n;++i)
+        {
+            if(nums[i]!=0)
+            {
+                nums[left++]=nums[i];
+            }
+        }
+        while(left<n)
+        {
+            nums[left++]=0;
+        }
+    }
+}
