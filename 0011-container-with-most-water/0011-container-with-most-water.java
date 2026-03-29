fc85299f870178927b1dad232e642051ee783787
Malaya Sahu <malaya.sahu@bounteous.com>
2026-03-29 12:24:33 +0530

    Time: 5 ms (82.8%), Space: 77.3 MB (79.31%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,17 +1,16 @@
 class Solution {
     public int maxArea(int[] height) {
+        
         int n=height.length;
+
         int ans=0;
         int i=0,j=n-1;
-        
-        while(i<j)
-        {
-            int area=Math.min(height[i],height[j])*(j-i);
-            ans=Math.max(ans,area);
-            if(height[i]<height[j])
-            i++;
-            else
-            j--;
+        while(i<j){
+            int len=Math.min(height[i],height[j]);
+
+            ans=Math.max(ans,(j-i)*len);
+            if(height[i]<height[j])i++;
+            else j--;
         }
         return ans;
     }
