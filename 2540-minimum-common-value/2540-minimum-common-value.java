301a0f2146e5e843b869c82f291550af96d232e8
Malaya Sahu <malaya.sahu@bounteous.com>
2026-05-20 10:30:04 +0530

    Time: 3 ms (33%), Space: 80.3 MB (79.77%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int n1=nums1.length;
+        int n2=nums2.length;
+        if(n1>n2)return getCommon(nums2,nums1);
+         int i=0,j=0;
+         int cnt=0;
+         while( i<n1 && j<n2)
+         {
+            if(nums1[i]==nums2[j])
+            return nums1[i];
+            while(i<n1 && j<n2 && nums1[i]==nums2[j])
+            {
+                i++;j++;
+            }
+            if(i<n1 && j<n2 && nums1[i]>nums2[j])
+            j++;
+            else
+            i++;
+         }
+         return -1;
+    }
+}
